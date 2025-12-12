bbbb51fe668e04286d429d8ef7776b3942c92163
Junaid abbasi <tedd.acebpo@gmail.com>
2025-12-12 02:41:53 +0000

    5

--- vite.config.ts
+++ vite.config.ts
@@ -6,7 +6,7 @@ export default defineConfig(({ mode }) => {
     const env = loadEnv(mode, '.', '');
 
     return {
-      base: '/Steel/',
+      base: '/STEEL/',
       server: {
         port: 3000,
         host: '0.0.0.0',
