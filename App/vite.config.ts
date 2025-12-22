867d60b214b27716df7368dbf71440d51e2f6946
Junaid abbasi <tedd.acebpo@gmail.com>
2025-12-22 16:26:09 -0500

    Modify Vite config for base path and clean imports
    Updated Vite configuration to set base path and removed unused imports.

--- App/vite.config.ts
+++ App/vite.config.ts
@@ -1,16 +1,7 @@
-import path from "path";
-import { defineConfig } from "vite";
-import react from "@vitejs/plugin-react";
+import { defineConfig } from 'vite'
+import react from '@vitejs/plugin-react'
 
 export default defineConfig({
-  server: {
-    port: 3000,
-    host: "0.0.0.0",
-  },
+  base: '/STEEL/',
   plugins: [react()],
-  resolve: {
-    alias: {
-      "@": path.resolve(__dirname, "."),
-    },
-  },
-});
+})
